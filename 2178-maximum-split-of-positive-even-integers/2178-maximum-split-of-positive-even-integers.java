92472291d5d4730ddd3400978e8f52ccfce57b3e
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-02-20 21:59:43 +0530

    Time: 133 ms (10.00%), Space: 196.8 MB (10.00%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -4,22 +4,29 @@ class Solution {
         if(finalSum%2!=0)
             return new ArrayList<>();
         long key=-1;
+        long max=0;
         for(long start=2;;start+=2){
             if(finalSum<=0)
                 break;
             long z=Long.min(start,finalSum);
             finalSum-=z;
             hm.put(z,hm.getOrDefault(z,0L)+1L);
+            max=Long.max(max,z);
             if(hm.get(z)>1){
                 key=z;
             }
         }
         List<Long> ans=new ArrayList<>();
-        for(Long z:hm.keySet())
-            ans.add(z);
-        Collections.sort(ans);
-        if(key!=-1){
-            ans.set(ans.size()-1,ans.get(ans.size()-1)+key);
+        for(Long z:hm.keySet()){
+            if(key==-1){
+                ans.add(z);
+            }else{
+                if(z==max){
+                    ans.add(z+key);
+                }else{
+                    ans.add(z);
+                }
+            }
         }
         return ans;
     }
