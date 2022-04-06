bce47b5787394a314632dff922689b421cfc335f
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-04-06 21:20:55 +0530

    Time: 16 ms (81.37%), Space: 42.4 MB (79.04%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,34 +1,32 @@
 class Solution {
+    int ans=0;
     public int maxLength(List<String> arr) {
-        int ans=0;
-        int total=arr.size();
-        for(int i=1;i<(1<<total);i++){
-            int dp[]=new int[26];
-            int tl=0;
-            int len=arr.size()-1;
-            for(int j=0;j<32;j++){
-                int k=i>>j;
-                if((k&1)==1){
-                    tl+=arr.get(len).length();
-                    for(int l=0;l<arr.get(len).length();l++){
-                        dp[arr.get(len).charAt(l)-'a']+=1;
-                    }
-                }
-                --len;
-                if(len<0)
-                    break;
-            }
-            boolean rs=true;
-            for(int x:dp){
-                if(x>1){
-                    rs=false;
-                    break;
-                }
-            }
-            if(rs){
-                ans=Integer.max(ans,tl);
-            }
+        int dp[]=new int[26];
+        for(int i=0;i<arr.size();i++){
+            Arrays.fill(dp,0);
+            dfs(i,arr,dp,0);
         }
         return ans;
+    }
+    void dfs(int i,List<String> ar,int[] dp,int len){
+        for(int j=0;j<26;j++){
+            if(dp[j]>1)
+            {
+                return;
+            }
+        }
+        ans=Integer.max(ans,len);
+         if(i==ar.size()){
+            return;
+        }
+        for(int j=0;j<ar.get(i).length();j++){
+            dp[ar.get(i).charAt(j)-'a']+=1;
+        }
+        for(int j=i+1;j<=ar.size();j++){
+            dfs(j,ar,dp,len+ar.get(i).length());
+        }
+        for(int j=0;j<ar.get(i).length();j++){
+            dp[ar.get(i).charAt(j)-'a']-=1;
+        }
     }
 }
