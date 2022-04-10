e6173de964fff36ca5166c5739d3c521bb252e72
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-04-10 12:21:19 +0530

    Time: 5 ms (50.00%), Space: 41 MB (100.00%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,23 +1,23 @@
 class Solution {
     public int largestInteger(int num) {
         char ar[]=(num+"").toCharArray();
-        ArrayList<Integer> a=new ArrayList<Integer>(),b=new ArrayList<>(),c=new ArrayList<>(),d=new ArrayList<>();
+        ArrayList<Integer> a=new ArrayList<Integer>(),c=new ArrayList<>();
         for(int i=0;i<ar.length;i++){
             if((ar[i]-'0')%2==0){
                 a.add(ar[i]-'0');
-                b.add(i);
             }else{
                 c.add(ar[i]-'0');
-                d.add(i);
             }
         }
         Collections.sort(a,Collections.reverseOrder());
         Collections.sort(c,Collections.reverseOrder());
-        for(int i=0;i<b.size();i++){
-            ar[b.get(i)]=(char)(a.get(i)+'0');
-        }
-        for(int i=0;i<d.size();i++){
-            ar[d.get(i)]=(char)(c.get(i)+'0');
+        int i1=0,i2=0;
+        for(int i=0;i<ar.length;i++){
+            if((ar[i]-'0')%2==0){
+                ar[i]=(char)(a.get(i1++)+'0');
+            }else{
+                ar[i]=(char)(c.get(i2++)+'0');
+            }
         }
         num=0;
         for(char x:ar){
