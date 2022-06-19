d8c7b3f37c834974a30c2f3bcfe292de48bc0a26
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-06-19 20:31:06 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (100.00%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -3,37 +3,20 @@ class Solution {
         if (num == 0) {
             return 0;
         }
-        HashSet<Integer> set = new HashSet<Integer>();
-        int x = k;
-        while (!set.contains(x%10)) {
-            set.add(x%10);
-            x+=k;
-        }
-        if (!set.contains(num%10)) {
+        if (k==0) {
+            if (num % 10 ==0 ){
+                return 1;
+            }
             return -1;
         }
-        set.clear();
-        for (int i=0; i<3001; i++) {
-           set.add(i*10+k);
-        }
-        int dp[] = new int[num+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        for(int i=0; i<dp.length;i++){
-            if(i==k){
-                dp[i]=1;
-            }else{  
-                for(Integer z : set){
-                    int rem = i-z;
-                    if(rem==0){
-                        dp[i]=Integer.min(dp[i],1);
-                    }else if(rem>=0 && dp[rem] != Integer.MAX_VALUE && dp[rem]>0) {
-                        dp[i]=Integer.min(dp[i], dp[rem]+1);
-                    }
-                }    
+        int x = k, i = 1;
+        while(x<=num) {
+            if(x%10 == num%10) {
+                return i;
             }
+            i+=1;
+            x = k*i;
         }
-        // System.out.println(set);
-        // System.out.println(Arrays.toString(dp));
-        return dp[num] == Integer.MAX_VALUE?-1:dp[num];
+        return -1;        
     }
 }
