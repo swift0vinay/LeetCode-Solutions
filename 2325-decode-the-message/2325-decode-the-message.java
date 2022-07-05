164cb2aff15b262f7822ea0090db3d30ddfe2da9
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-07-05 21:34:25 +0530

    Time: 4 ms (91.14%), Space: 42.2 MB (96.21%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -2,7 +2,6 @@ class Solution {
     public String decodeMessage(String key, String message) {
         boolean used[] = new boolean[26];
         char mk[] = new char[26];
-        char mkk[] = new char[26];
         int ind=0;
         for(char x:key.toCharArray()) {
             if(x==' ')
@@ -10,21 +9,18 @@ class Solution {
             if(used[x-'a']){
                 continue;
             }else{
-                mk[ind++] = x;
+                mk[x-'a']=(char)(ind+'a');
+                ++ind;
                 used[x-'a'] = true;
             }
         }
-        for (int i=0;i<26;i++) {
-            mkk[mk[i]-'a']=(char)(i+'a');
-        }
-        System.out.println(Arrays.toString(mk));
         StringBuilder ans=new StringBuilder();
         for(char x:message.toCharArray()) {
             if(x==' '){
                 ans.append(' ');
                 continue;
             }
-            ans.append(mkk[x-'a']);
+            ans.append(mk[x-'a']);
         }
         return ans.toString();
     }
