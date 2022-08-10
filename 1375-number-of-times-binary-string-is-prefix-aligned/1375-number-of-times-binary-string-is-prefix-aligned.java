4dcae0569c07c46f3408fc2128decb1fe5597e54
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2022-08-10 18:23:37 +0530

    Time: 2 ms (98.51%), Space: 63.6 MB (79.55%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
@@ -1,13 +1,9 @@
 class Solution {
     public int numTimesAllBlue(int[] l) {
-        int ans=0;
-        int right=0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int ans=0,max=0;
         for(int i=0;i<l.length;i++){
-            pq.add(l[i]);
-            while(!pq.isEmpty() && pq.peek()<=i+1)
-                pq.poll();
-            if(pq.size()==0)
+            max=Integer.max(max,l[i]);
+            if(max==i+1)
                 ++ans;
         }
         return ans;
